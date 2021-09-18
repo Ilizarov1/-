8c92aa54bf1f28018e78b39b43054da7cfc987c8
ilizarov1 <1224722105@qq.com>
2021-09-18 15:40:10 +0800

    offer 05

--- 每日一题/剑指 Offer 03. 数组中重复的数字.py
+++ 每日一题/剑指 Offer 03. 数组中重复的数字.py
@@ -13,6 +13,7 @@ class Solution:
                 hashmap[num] += 1
         return -1
 
+    # 交换索引位置，形成nums[x]=x,如果再次满足nums[x]=x则重复
     def bestSolution(self, nums: List[int]) -> int:
         i = 0
         while i < len(nums):
---
+++ 每日一题/剑指 Offer 05. 替换空格.py
@@ -0,0 +1,4 @@
+# https://leetcode-cn.com/problems/ti-huan-kong-ge-lcof/
+class Solution:
+    def replaceSpace(self, s: str) -> str:
+        return s.replace(" ", "%20")
