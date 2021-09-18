025d6bb90be504e4f930de4e2e187d2c9939279f
ilizarov1 <1224722105@qq.com>
2021-09-18 15:34:18 +0800

    offer 03

---
+++ 每日一题/剑指 Offer 03. 数组中重复的数字.py
@@ -0,0 +1,26 @@
+# https://leetcode-cn.com/problems/shu-zu-zhong-zhong-fu-de-shu-zi-lcof/
+from typing import List
+import collections
+
+
+class Solution:
+    def findRepeatNumber(self, nums: List[int]) -> int:
+        hashmap = collections.defaultdict(int)
+        for num in nums:
+            if hashmap[num] >= 1:
+                return num
+            else:
+                hashmap[num] += 1
+        return -1
+
+    def bestSolution(self, nums: List[int]) -> int:
+        i = 0
+        while i < len(nums):
+            if nums[i] == i:
+                i += 1
+                continue
+            if nums[nums[i]] == nums[i]:
+                return nums[i]
+            else:
+                nums[nums[i]], nums[i] = nums[i], nums[nums[i]]
+        return -1
