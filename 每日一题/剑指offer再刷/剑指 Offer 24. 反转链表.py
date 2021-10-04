d12f2ae0aef7efc21c9b590fd8f740e1ed8aea97
ilizarov1 <1224722105@qq.com>
2021-10-04 16:21:11 +0800

    offer 24

---
+++ 每日一题/剑指offer再刷/剑指 Offer 24. 反转链表.py
@@ -0,0 +1,22 @@
+# https://leetcode-cn.com/problems/fan-zhuan-lian-biao-lcof/
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def reverseList(self, head: ListNode) -> ListNode:
+        pre = None
+        cur = head
+        # if head.next:
+        #     cur=head.next
+        # else:
+        #     return head
+        while cur:
+            tmp = cur
+            cur = cur.next
+            tmp.next = pre
+            pre = tmp
+        return pre
