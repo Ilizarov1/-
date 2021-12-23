12b46e36d34ae29e8f264d5a27d7d0bf7b290661
Ilizarov1 <1224722105@qq.com>
2021-12-23 20:59:35 +0800

    hot100 3

---
+++ hot100/3. 无重复字符的最长子串.py
@@ -0,0 +1,23 @@
+# https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
+
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        length = len(s)
+        hm = {}
+        start = 0
+        ret = 0
+        for end in range(length):
+            w = s[end]
+            if w in hm:
+                start = max(hm[w] + 1, start)
+                hm[w] = end
+            else:
+                hm[w] = end
+            ret = max(ret, end - start + 1)
+            print(s[start:end+1])
+        return ret
+
+
+x = Solution()
+print(x.lengthOfLongestSubstring("pwwkew"))
