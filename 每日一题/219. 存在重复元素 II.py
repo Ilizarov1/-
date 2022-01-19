442de0c479fa65d1d05859a7363dbd513817d37e
Ilizarov1 <1224722105@qq.com>
2022-01-19 14:48:24 +0800

    219

---
+++ 每日一题/219. 存在重复元素 II.py
@@ -0,0 +1,15 @@
+# https://leetcode-cn.com/problems/contains-duplicate-ii/
+from typing import List
+
+
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        n = len(nums)
+        s = set()
+        for i in range(n):
+            if i > k:
+                s.remove(nums[i-k-1])
+            if nums[i] in s:
+                return True
+            s.add(nums[i])
+        return False
