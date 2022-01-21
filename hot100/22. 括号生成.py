c4da0480a2a2e5d8afbb0b8595ac51edcdc2d4f0
Ilizarov1 <1224722105@qq.com>
2022-01-21 16:21:56 +0800

    22

---
+++ hot100/22. 括号生成.py
@@ -0,0 +1,20 @@
+# https://leetcode-cn.com/problems/generate-parentheses/
+from typing import List
+
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ret = []
+
+        def recur(cur, left, right):
+            if left == 0 and right == 0:
+                ret.append(cur)
+                return
+            if right < left:
+                return
+            if left > 0:
+                recur(cur+'(', left-1, right)
+            if right > 0:
+                recur(cur+')', left, right-1)
+        recur("", n, n)
+        return ret
