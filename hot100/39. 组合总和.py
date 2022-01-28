3b2f68df7845ca0960c380d2186db7db0f5967a6
Ilizarov1 <1224722105@qq.com>
2022-01-28 16:16:39 +0800

    39

---
+++ hot100/39. 组合总和.py
@@ -0,0 +1,23 @@
+# https://leetcode-cn.com/problems/combination-sum/
+import functools
+from typing import List
+
+
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        ret = []
+        size = len(candidates)
+        if size == 0:
+            return []
+
+        @functools.lru_cache(None)
+        def dfs(begin, target, path):
+            if target < 0:
+                return
+            if target == 0:
+                ret.append(path)
+                return
+            for idx in range(begin, size):
+                dfs(idx, target - candidates[idx], path+[candidates[idx]])
+        dfs(0, target, [])
+        return ret
