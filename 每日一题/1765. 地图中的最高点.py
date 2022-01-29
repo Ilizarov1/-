78056c2b9eaea05059d8d06fe8b09c2b6cb45f8b
Ilizarov1 <1224722105@qq.com>
2022-01-29 13:58:00 +0800

    1765

---
+++ 每日一题/1765. 地图中的最高点.py
@@ -0,0 +1,29 @@
+# https://leetcode-cn.com/problems/map-of-highest-peak/
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        m, n = len(isWater), len(isWater[0])
+        ret = [[0]*n for i in range(m)]
+        q = deque()
+        for i in range(m):
+            for j in range(n):
+                if isWater[i][j]:
+                    q.append((i, j))
+                else:
+                    ret[i][j] = -1
+        h = 1
+        dirs = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+        while q:
+            d = len(q)
+            for _ in range(q):
+                x, y = q.popleft()
+                for dir in dirs:
+                    nx, ny = x+dir[0], y+dir[1]
+                    if 0 <= nx < m and 0 <= ny < n and isWater[nx][ny] == 0:
+                        ret[nx][ny] = h
+                        q.append((nx, ny))
+            h += 1
+        return ret
