da1d5d25b5d8bdb313485dfa5d73444e4e04b92b
Ilizarov1 <1224722105@qq.com>
2022-02-08 15:43:11 +0800

    75

---
+++ hot100/75. 颜色分类.py
@@ -0,0 +1,23 @@
+# https://leetcode-cn.com/problems/sort-colors/
+from typing import List
+
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        p0, p2, idx = 0, n-1, 0
+        while idx <= p2:
+            while idx <= p2 and nums[idx] == 2:
+                nums[idx], nums[p2] = nums[p2], nums[idx]
+                p2 -= 1
+            if nums[idx] == 0:
+                nums[idx], nums[p0] = nums[p0], nums[idx]
+                p0 += 1
+            idx += 1
+
+
+x = Solution()
+print(x.sortColors([2, 0, 2, 1, 1, 0]))
