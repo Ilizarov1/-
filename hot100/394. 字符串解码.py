090be7eb9e8dffa77e60ae672318d9cde79fcde5
Ilizarov1 <1224722105@qq.com>
2022-02-13 20:51:50 +0800

    394

---
+++ hot100/394. 字符串解码.py
@@ -0,0 +1,16 @@
+# https://leetcode-cn.com/problems/decode-string/
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stk, ret, multi = [], '', 0
+        for i in s:
+            if i == '[':
+                stk.append([multi, ret])
+                multi, ret = 0, ''
+            if i == ']':
+                cur_multi, last_ret = stk.pop()
+                ret = last_ret + cur_multi * ret
+            if i >= '0' and i <= '9':
+                multi = multi * 10 + int(i)
+            if i >= 'a' and i <= 'z':
+                ret += i
+        return ret
