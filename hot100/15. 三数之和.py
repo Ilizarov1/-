a564e3eff0298057c2205612720e97e0826e145c
Ilizarov1 <1224722105@qq.com>
2022-03-08 19:55:50 +0800

    加一点注释

--- hot100/15. 三数之和.py
+++ hot100/15. 三数之和.py
@@ -10,23 +10,30 @@ class Solution:
         nums.sort()
         ret = []
         for i in range(n):
+            # 已排序，大于0则不可能存在三数和等于0
             if nums[i] > 0:
                 return ret
+            # 防止重复
             if i > 0 and nums[i] == nums[i-1]:
                 continue
+            # 双指针 计算，由两边逼近
             left = i+1
             right = n-1
             while left < right:
                 if nums[i] + nums[left] + nums[right] == 0:
+                    # 加入一个答案
                     ret.append([nums[i], nums[left], nums[right]])
+                    # 继续判断是否含有其他答案，去除重复解
                     while left < right and nums[left] == nums[left+1]:
                         left += 1
                     while left < right and nums[right] == nums[right-1]:
                         right -= 1
                     left += 1
                     right -= 1
+                # 太大，右边界移动
                 elif nums[i]+nums[left]+nums[right] > 0:
                     right -= 1
+                # 太小，左边界移动
                 else:
                     left += 1
         return ret
--- hot100/3. 无重复字符的最长子串.py
+++ hot100/3. 无重复字符的最长子串.py
@@ -1,6 +1,6 @@
 # https://leetcode-cn.com/problems/longest-substring-without-repeating-characters/
 
-
+# 滑动窗口
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
         length = len(s)
@@ -10,6 +10,7 @@ class Solution:
         for end in range(length):
             w = s[end]
             if w in hm:
+                # 更新起始索引,如果重复的字符在start之前就不进行更新
                 start = max(hm[w] + 1, start)
                 hm[w] = end
             else:
