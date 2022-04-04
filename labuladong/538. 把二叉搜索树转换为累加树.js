7b9ba99f41d0c775ac340612c3339f8227fc385f
Ilizarov1 <1224722105@qq.com>
2022-04-04 17:41:27 +0800

    538

---
+++ labuladong/538. 把二叉搜索树转换为累加树.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+function TreeNode(val, left, right) {
+    this.val = val === undefined ? 0 : val;
+    this.left = left === undefined ? null : left;
+    this.right = right === undefined ? null : right;
+}
+/**https://leetcode-cn.com/problems/convert-bst-to-greater-tree/
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var convertBST = function (root) {
+    let sum = 0;
+    (function traverse(root) {
+        // 逆序遍历
+        if (root == null) return;
+        traverse(root.right);
+        sum += root.val;
+        root.val = sum;
+        traverse(root.left);
+    })(root);
+};
