d362948331b0e79093458e0541cc961b5357c397
Ilizarov1 <1224722105@qq.com>
2023-07-30 19:34:58 +0800

    getType

---
+++ 一些面试题/getType.ts
@@ -0,0 +1,17 @@
+const class2type = {};
+
+'Boolean Number String Function Array Date RegExp Object Error'.split(' ').map((item, index) => {
+    class2type[`[object ${item}]`] = item.toLowerCase();
+});
+
+function getType(obj: any) {
+    if (obj == null) {
+        return obj + '';
+    }
+
+    return typeof obj === 'object' || typeof obj === 'function'
+        ? class2type[Object.prototype.toString.call(obj)] || 'object'
+        : typeof obj;
+}
+
+console.log(getType(undefined));
